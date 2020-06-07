40a4fee914a1ab82fe900294188a63d70c3d24ee
Narek188 <narek-sargsyan-01@inbox.ru>
2020-06-07 21:34:22 +0400

    adding_price_range

---
+++ js/user_plans.js
@@ -0,0 +1,14 @@
+var range = document.getElementById('range'),
+    rangeV = document.getElementById('rangeV'),
+
+setValue = ()=>{
+        var newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) ),
+            newPosition = 10 - (newValue * 0.2);
+
+        rangeV.innerHTML = "<span>"+range.value+"</span>";
+        // rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
+        rangeV.style.left = "calc("+newValue+"% + ("+newPosition+"px))";
+    };
+    
+document.addEventListener("DOMContentLoaded", setValue);
+range.addEventListener('input', setValue);
