853a537c0b6c5e8ae4cfb04af7bacff9c9a54743
Narek188 <narek-sargsyan-01@inbox.ru>
2020-06-14 19:29:11 +0400

    start_user_page_contacts

--- js/user_page.js
+++ js/user_page.js
@@ -1,15 +1,52 @@
-$("#change_name").on('click', function () {    
-    if (!$(this).hasClass('fa-times')) {
-        $(this).parent().prev().find('span').hide();
-        $(this).parent().prev().find('input').show();
-        $(this).attr('class', 'fa fa-times');
-    } else {
-        $(this).parent().prev().find('span').show();
-        $(this).parent().prev().find('input').hide();
-        $(this).attr('class', 'fa fa-pencil');
-    }    
+$(".contacts-container table select").find('option').each(function (e) {
+    if ($(this).text() == $(".contacts-container table select").next().text()) {
+        $(this).attr('selected', true);
+    }
 });
 
-$('.change_inp').on('input', function () {
-    console.log($(this).parent().next().html('<i class="fa fa-check" aria-hidden="true"></i>'));
+$(".contacts-container table").find('i').each(function () {
+    $(this).on('click', function () {
+        if (!$(this).hasClass('fa-times')) {
+            if ($(this).attr('id') == 'select_country') {
+                $(this).parent().prev().find('select').show();
+                $(this).parent().prev().find('span').hide();
+                $(this).attr('class', 'fa fa-times');
+            } else if ($(this).attr('id') == 'select_gender') {
+                $(".contacts-container table #gender_td label").css('display', 'block');
+                $(this).parent().prev().find('input').show();
+                $(this).parent().prev().find('span').hide();
+            } else {
+                $(this).parent().prev().find('input').val($(this).parent().prev().find('span').text());
+                $(this).parent().prev().find('span').hide();
+                $(this).parent().prev().find('input').show();
+                $(this).attr('class', 'fa fa-times');
+            }
+        } else {
+            $(this).parent().prev().find('span').show();
+            $(this).parent().prev().find('span').prev().hide();
+            $(this).attr('class', 'fa fa-pencil');
+        }    
+    });
 });
+
+// $("#change_name").on('click', function () {    
+//     if (!$(this).hasClass('fa-times')) {
+//         $(this).parent().prev().find('span').hide();
+//         $(this).parent().prev().find('input').show();
+//         $(this).attr('class', 'fa fa-times');
+//     } else {
+//         $(this).parent().prev().find('span').show();
+//         $(this).parent().prev().find('input').hide();
+//         $(this).attr('class', 'fa fa-pencil');
+//     }    
+// });
+
+// $('.change_inp').on('input', function () {
+//     $(this).parent().next().html('<i class="fa fa-check" id="save_change" aria-hidden="true"></i>');
+
+//     $(this).parent().next().find('i').on('click', function () {
+//         $(this).parent().prev().find('span').show();
+//         $(this).parent().prev().find('input').hide();
+//         $(this).attr('class', 'fa fa-pencil');
+//     });
+// });
