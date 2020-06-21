173fb96842a1afd6cb3ebc5603c134446145ffca
Narek188 <narek-sargsyan-01@inbox.ru>
2020-06-21 13:38:25 +0400

    finish_user_page_template

--- js/user_page.js
+++ js/user_page.js
@@ -6,20 +6,44 @@ $(".contacts-container table select").find('option').each(function (e) {
 
 $(".contacts-container table").find('i').each(function () {
     $(this).on('click', function () {
-        if (!$(this).hasClass('fa-times')) {
+        if ($(this).hasClass('fa-pencil')) {
             if ($(this).attr('id') == 'select_country') {
                 $(this).parent().prev().find('select').show();
                 $(this).parent().prev().find('span').hide();
                 $(this).attr('class', 'fa fa-times');
             } else if ($(this).attr('id') == 'select_gender') {
-                $(".contacts-container table #gender_td label").css('display', 'block');
-                $(this).parent().prev().find('input').show();
+                $(".contacts-container table #gender_td .gender_container").show();
+                $(".contacts-container table #gender_td .gender_container input").show();
+
                 $(this).parent().prev().find('span').hide();
+                $(this).attr('class', 'fa fa-times');
             } else {
                 $(this).parent().prev().find('input').val($(this).parent().prev().find('span').text());
                 $(this).parent().prev().find('span').hide();
                 $(this).parent().prev().find('input').show();
                 $(this).attr('class', 'fa fa-times');
+            }
+        } else if ($(this).hasClass('fa-check')) {
+            if (!$(this).parent().prev().find('input').length) {
+                $(this).parent().prev().find('select').hide();
+                $(this).parent().prev().find('span').text($(this).parent().prev().find('select option:selected').text());
+            } else {
+                if ($(this).parent().prev().find('input').attr('type') == 'radio') {
+                    if ($(this).parent().prev().find('input:checked').attr('id') == 'female') {
+                        $(this).parent().prev().find('span').text('Female');
+                    } else if ($(this).parent().prev().find('input:checked').attr('id') == 'male') {
+                        $(this).parent().prev().find('span').text('Male');
+                    }
+
+                    $(".contacts-container table #gender_td .gender_container").hide();
+                    $(this).parent().prev().find('span').show();
+                } else {
+                    $(this).parent().prev().find('input').hide();
+                    $(this).parent().prev().find('span').text($(this).parent().prev().find('input').val());
+                    $(this).parent().prev().find('span').show();
+                }
+
+                $(this).attr('class', 'fa fa-pencil');
             }
         } else {
             $(this).parent().prev().find('span').show();
@@ -29,24 +53,20 @@ $(".contacts-container table").find('i').each(function () {
     });
 });
 
-// $("#change_name").on('click', function () {    
-//     if (!$(this).hasClass('fa-times')) {
-//         $(this).parent().prev().find('span').hide();
-//         $(this).parent().prev().find('input').show();
-//         $(this).attr('class', 'fa fa-times');
-//     } else {
-//         $(this).parent().prev().find('span').show();
-//         $(this).parent().prev().find('input').hide();
-//         $(this).attr('class', 'fa fa-pencil');
-//     }    
-// });
+$(".contacts-container table").find('input').each(function () {
+    $(this).on('input', function () {
+        $(this).parent().next().find('i').attr('class', 'fa fa-check');
+    });
+});
 
-// $('.change_inp').on('input', function () {
-//     $(this).parent().next().html('<i class="fa fa-check" id="save_change" aria-hidden="true"></i>');
+$(".contacts-container table").find('select').each(function () {
+    $(this).on('change', function () {
+        $(this).parent().next().find('i').attr('class', 'fa fa-check');
+    });
+});
 
-//     $(this).parent().next().find('i').on('click', function () {
-//         $(this).parent().prev().find('span').show();
-//         $(this).parent().prev().find('input').hide();
-//         $(this).attr('class', 'fa fa-pencil');
-//     });
-// });
+$(".contacts-container table #gender_td .gender_container").find('input').each(function () {
+    $(this).on('change', function () {
+        $(this).parent().parent().parent().next().find('i').attr('class', 'fa fa-check');
+    });
+});
